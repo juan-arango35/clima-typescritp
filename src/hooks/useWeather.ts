cf2ef4ee299ba7b7f0c268a38bf97cda9eb63d40
juan arango <juanarangozapata35@gmail.com>
2025-01-06 19:05:49 -0500

    agrega nueva rama

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,21 +1,19 @@
 import { useState } from "react";
 import { SearchType } from "../types";
-import {  z } from "zod";
+import { z } from "zod";
 /*import { useWeatherType } from '../components/form/Form';
  */
 
-
-
 export const Weather = z.object({
-    name: z.string(),
-    main: z.object({
-      temp: z.number(),
-      temp_min: z.number(),
-      temp_max: z.number(),
-    })
-}) 
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_min: z.number(),
+    temp_max: z.number(),
+  }),
+});
 
- type Weather = z.infer<typeof Weather> 
+type Weather = z.infer<typeof Weather>;
 
 const intialState = {
   name: "",
@@ -24,50 +22,55 @@ const intialState = {
     temp_min: 0,
     temp_max: 0,
   },
-}
-
+};
 
 const useWeather = () => {
-  const [weather, setWeather] = useState<Weather>(intialState)
+  const [weather, setWeather] = useState<Weather>(intialState);
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+
   const fecthWather = async (search: SearchType) => {
-   
     const appId = import.meta.env.VITE_API_KEY;
-    setLoading(true)
-    setWeather(intialState)
+    setLoading(true);
+    setWeather(intialState);
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
       const result = await fetch(geoUrl);
       const data = await result.json();
-
-     const climaData= await data[0]
-     const lat = climaData.lat;
-     const lon = climaData.lon;
-   
-     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`)
-     const dataClima = await response.json()
-     const climaNubes = await dataClima
-     const {name, main:{temp, temp_min, temp_max}} = climaNubes
-     setWeather({name, main:{temp, temp_min, temp_max}})
      
-     console.log(name,"temp:", temp,"temp min:", temp_min,"temp max :", temp_max)
-    } catch (error) { 
+
+      const climaData = await data[0];
+      console.log("datos del clima",climaData);
+   
+      const lat = climaData.lat;
+      const lon = climaData.lon;
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+      );
+      const dataClima = await response.json();
+     /*  console.log(dataClima); */
+      const climaNubes = await dataClima;
+    
+    
+      const {
+        name,
+        main: { temp, temp_min, temp_max },
+      } = climaNubes;
+      setWeather({ name, main: { temp, temp_min, temp_max } });
+    } catch (error) {
       console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
 
   //funcion para mostar el componente clima si esuq existe algo
 
-  
   return {
-    weather, 
+    weather,
     loading,
-    fecthWather
-  
-
+    fecthWather,
   };
 };
 
